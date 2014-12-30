bb45f27e58a87fdf5baa7621a4ee0ffe7fce084e
kcamrine <kc.amrine@gmail.com>
2014-12-30 10:36:11 -0800

    clean code again

--- match-metabolites.R
+++ match-metabolites.R
@@ -5,31 +5,37 @@
 ### berries.metabolites
 
 ## We shall call this iterative fuzzy mapping of metabolite characterizations
-## SET YOUR PARAMETERS HERE
+
+######### SET YOUR PARAMETERS HERE #################
 
 # make this the file you want to match. Right now, it codes the example file
 # so you can look at what the format should be for the input file
 unknown.metabolites <- read.table("unknown_metabolites_clean.txt") 
+
 # this file should stay the same,unless you have something else to match to. If that's the 
 # case, make sure that the format is the same.
 # this file has all of the metabolites. It will take a long time, especially if you
 # have shorter matches, for example "urea" will match LOTS of stuff perfectly
 # so you may want to clean this up before you run it. 
 reference.metabolites <- read.table("metabolites_list_processed_clean.txt") 
+
 # how lenient you want to be. 0.1 is 10% difference
 subs = 0.1
 
+########### Done setting parameters ##################
+
+
 unknown.metabolites$V3 <- as.character(unknown.metabolites$V3)
 
+filename <- paste("match-metabolites",subs,"out","txt",sep=".")
+sink(filename) #right now, output just sinks to a file. feel free to change
 
-## different approach
-sink("NEWESTyourmatches_1per.txt")
 matchtimes <- c()
 for(i in 1:length(unknown.metabolites[,1])){
-  matchtimes <- c(matchtimes,0);
+  matchtimes <- c(matchtimes,0); #records num of matches for each query
   teststring <- unknown.metabolites[i,3]
   substitutions <- 0
-  matchstring <- rep(0,length(reference.metabolites[,1]))
+  matchstring <- rep(0,length(reference.metabolites[,1])) #records what is matched
   matches <- c();
   while(matchtimes[i] < 1 & substitutions < subs){
     for(querynum in 1:length(reference.metabolites[,1])){
@@ -37,29 +43,28 @@ for(i in 1:length(unknown.metabolites[,1])){
       match <- agrep(query,teststring,max.distance = 0,ignore.case=TRUE)
       if(length(match) > 0){
         if(matchstring[querynum] == 1){
-          next;
+          next; #go on if it already matched this met
         }
         if(nchar(query) <= 4 & substitutions > 0){
-          next;
+          next; #go on if it is less or equal to 4 (crap match)
         }        
         if(!(query %in% matches)){
           matchtimes[i] = matchtimes[i]+1;
           matches <- c(matches,query)
         }
         ## print the match
-        matched.yay <- paste(query,unknown.metabolites[i,1],as.character(reference.metabolites[querynum,2]),as.character(reference.metabolites[querynum,3]), as.character(reference.metabolites[querynum,4]), as.character(reference.metabolites[querynum,5]), as.character(reference.metabolites[querynum,6]),substitutions,teststring, sep= "\t")
-        cat(matched.yay,"\n")
+        matched.yay <- paste(query,unknown.metabolites[i,1],
+		    as.character(reference.metabolites[querynum,2]),
+		    as.character(reference.metabolites[querynum,3]), 
+		    as.character(reference.metabolites[querynum,4]), 
+		    as.character(reference.metabolites[querynum,5]), 
+		    as.character(reference.metabolites[querynum,6]),
+		    substitutions,teststring, sep= "\t")
+        cat(matched.yay,"\n") #print the match
         matchstring[querynum] = 1
       }
     }
-    substitutions = substitutions+0.1
+    substitutions = substitutions+0.1 #make more lenient
   }
 }
-sink()
-
-## start the function. Right now it is sent to print. We need to store the data. 
-## I suggest sinking the prints into a file because of the recursion. I'll worry about 
-## it later. 
-#sink("metabolite_output_plus_all_shit-recursive2.txt")
-#match.metabolites(unknown.metabolites)
-#sink()
+sink() #stop the sinking
